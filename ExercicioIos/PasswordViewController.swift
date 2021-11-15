b120260501c547d828ea13b293b18de1b4459249
Renan <11900334@aluno.cotemig.com.br>
2021-11-15 15:26:29 -0300

    ajustes

--- ExercicioIos/PasswordViewController.swift
+++ ExercicioIos/PasswordViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PasswordViewController: UIViewController {
 
@@ -17,7 +18,41 @@ class PasswordViewController: UIViewController {
     }
     
 
-    @IBAction func btnRecuperar(_ sender: Any) {
+    @IBAction func btnPassword(_ sender: Any) {
+        let email: String = txtEmail.text ?? ""
+        print(email)
+        if(email == "") {
+            self.showToast(message: "Por favor digite seu email")
+        }else {
+            Auth.auth().sendPasswordReset(withEmail: email, completion: {
+                error in
+                if error != nil {
+                    self.showToast(message: "\(error)")
+                }else {
+                    self.showToast(message: "Link enviado no email")
+                }
+            })
+        }
+        
+    }
+    
+    func showToast(message : String) {
+
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center;
+        toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
     }
     /*
     // MARK: - Navigation
