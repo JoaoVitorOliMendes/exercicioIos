64fe02d26344104e62c96fa397bf7bbe86e08d42
Renan <11900334@aluno.cotemig.com.br>
2021-11-15 11:58:40 -0300

    initial commit

---
+++ ExercicioIos/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  ExercicioIos
+//
+//  Created by COTEMIG on 15/11/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtSenha: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func btnLogin(_ sender: Any) {
+    }
+    @IBAction func btnRegister(_ sender: Any) {
+    }
+    @IBAction func btnForgotPass(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ExercicioIos/RegisterViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RegisterViewController.swift
+//  ExercicioIos
+//
+//  Created by COTEMIG on 15/11/21.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtSenha: UITextField!
+    @IBOutlet weak var txtConfirmarSenha: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func btnCad(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
